8fac7f8ea702ed2af68370e0a2a7310a1671e61f
lingkung <qazaa002@gmail.com>
2019-05-29 15:48:58 +0700

    add logout

---
+++ src/app/logout/logout.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { auth } from 'firebase/app'
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.page.html',
+  styleUrls: ['./logout.page.scss'],
+})
+export class LogoutPage implements OnInit {
+
+  constructor(public afAuth: AngularFireAuth, public user: UserService, public router: Router) { 
+    this.afAuth.auth.signOut();
+  }
+
+  ngOnInit() {
+  }
+
+}
